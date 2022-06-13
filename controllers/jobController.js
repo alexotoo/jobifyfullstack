2c171e20411e1dc2696fc998c5c1b695d2ddf017
Alex Otoo <alexotoous@outlook.com>
2022-06-13 14:22:53 -0400

    server side get all jobs done

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -14,15 +14,25 @@ const createJob = async (req, res) => {
   res.status(StatusCodes.CREATED).json({ job });
 };
 
+//delete jobs
 const deleteJob = async (req, res) => {
   res.send("delete jobs");
 };
+
+//get all jobs
 const getAllJobs = async (req, res) => {
-  res.send("hellooo all jobs");
+  const jobs = await Job.find({ createdBy: req.user.userId });
+  res
+    .status(StatusCodes.OK)
+    .json({ jobs, totalJobs: jobs.length, numOfPages: 1 });
 };
+
+//update jobs
 const updateJob = async (req, res) => {
   res.send("update job");
 };
+
+//show jobs stats
 const showStats = async (req, res) => {
   res.send("show job stats");
 };
